6bda5f96dfdc450fbb32801d708de80d9f472aec
Leanne <lealsatie@icloud.com>
2021-01-07 18:25:53 -0500

    Staging files

---
+++ Player/PlayerRB.gd
@@ -0,0 +1,10 @@
+extends RigidBody
+export var speed:=.05
+var velocity:Vector3
+
+func _physics_process(delta):
+	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	apply_central_impulse(velocity.normalized() * speed)
+
+
