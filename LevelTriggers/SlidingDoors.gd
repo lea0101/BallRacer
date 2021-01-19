85bc267c84cffc959f3b04cee971f043f596668d
Leanne <lealsatie@icloud.com>
2021-01-19 10:02:13 -0500

    close door

--- LevelTriggers/SlidingDoors.gd
+++ LevelTriggers/SlidingDoors.gd
@@ -7,3 +7,8 @@ onready var animator:=$AnimationPlayer
 func _on_SlidingDoors_body_entered(body):
 	if("Player" in body.name):
 		animator.play("OpenDoor")
+
+
+func _on_SlidingDoors_body_exited(body):
+	if("Player" in body.name):
+		animator.play("CloseDoor")
